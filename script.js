254f1085a1fbac0be7f87dce55dafb16ae6c7a09
davlsb <slawotsky@gmail.com>
2024-03-10 16:17:35 -0400

    set flexbox grid to 16x16 (256 divs), set it to be full screen

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let gridDiv = document.querySelector("#grid");
 
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < 256; i++){
     const div = document.createElement('div');
     div.classList.add("gridElement");
+    div.textContent = " ";
     console.log("added a div");
     gridDiv.appendChild(div);
 }
