22a4aa507a8a5b2916a9989f919259706fc87cfe
davlsb <slawotsky@gmail.com>
2024-03-10 16:35:13 -0400

    fix the grid elements so their sizes take the full screen/grid

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 let gridDiv = document.querySelector("#grid");
+let gridSize = 16;
 
-for(let i = 0; i < 256; i++){
+for(let i = 0; i < gridSize*gridSize; i++){
     const div = document.createElement('div');
     div.classList.add("gridElement");
     div.textContent = " ";
     console.log("added a div");
     gridDiv.appendChild(div);
+}
+
+function askGridSize(){
+    gridSize = prompt("What number x number should the grid be?");
+    gridDiv.innerHTML = ""; //erase existing grid
+
+    for(let i = 0; i < gridSize*gridSize; i++){
+        const div = document.createElement('div');
+        div.classList.add("gridElement");
+        div.textContent = "new div";
+        div.style.flexBasis = `${(100/gridSize)}%`;
+        div.style.height = `${(100/gridSize)}vh`;
+        console.log("added a new div");
+        gridDiv.appendChild(div);
+    }
+
 }
 
 gridDiv.addEventListener("mouseover", (event) => {
